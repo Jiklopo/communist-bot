7225caa355792ff9275a34ba0da8fe6cf0113d05
Timur Khaimovich <itima.com@gmail.com>
2020-05-26 14:11:59 +0600

    Playing audio but a lot of garbage

--- bot/music.py
+++ bot/music.py
@@ -1,22 +1,62 @@
-from discord import Message, FFmpegOpusAudio
-from discord.ext import commands
+from discord import FFmpegOpusAudio
+from discord.ext import commands as cmd
+import youtube_dl as yt
+import os
 
 
-class Music(commands.Cog):
-    def __init__(self, bot: commands.Bot):
+class Music(cmd.Cog):
+    def __init__(self, bot: cmd.Bot):
         self.bot = bot
+        self.ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'opus',
+                'preferredquality': 64
+            }]}
+        self.ydl = yt.YoutubeDL(self.ydl_opts)
 
-    @commands.command()
-    async def anthem(self, msg: Message):
+    @cmd.command()
+    async def anthem(self, ctx: cmd.Context):
         """Исполнить Гимн СССР в голосовом канале, в котором вы находитесь"""
         try:
-            ch = msg.author.voice.channel
+            ch = ctx.author.voice.channel
             voice = await ch.connect()
-            await msg.channel.send('Здравия желаю, товарищи!')
+            await ctx.channel.send('Здравия желаю, товарищи!')
             voice.play(FFmpegOpusAudio('resources/anthem.opus'))
         except AttributeError as e:
             print(e)
-            await msg.channel.send('Вы не подключены к голосовому каналу!')
+            await ctx.channel.send('Вы не подключены к голосовому каналу!')
         except Exception as e:
             print(e)
-            await msg.channel.send('У меня не получается!')
+            await ctx.channel.send('У меня не получается!')
+
+    @cmd.command()
+    async def play(self, ctx: cmd.Context, link):
+        try:
+            ch = ctx.author.voice.channel
+            voice = await ch.connect()
+            info = self.ydl.extract_info(link)
+            filename = f"{info['title']}-{info['id']}.opus"
+            voice.play(FFmpegOpusAudio(filename))
+        except AttributeError as e:
+            print(e)
+            await ctx.channel.send('Вы не подключены к голосовому каналу!')
+        except Exception as e:
+            print(e)
+            await ctx.channel.send('У меня не получается!')
+
+
+if __name__ == '__main__':
+    nyan = 'https://www.youtube.com/watch?v=QH2-TGUlwu4'
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'opus',
+            'preferredquality': 192
+        }]
+    }
+    ydl = yt.YoutubeDL(ydl_opts)
+    info = ydl.extract_info(nyan)
+    print(info)
